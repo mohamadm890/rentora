d487eff7c308509d5717627f429e69fef04f0f21
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-06-10 13:18:41 +0100

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -41,6 +41,7 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} dir={isRTL ? "rtl" : "ltr"} className={`${inter.variable} ${poppins.variable}`}>
+      <meta name="google-site-verification" content="fOr9peFwgeTwzGtSLbZDYQf3PAmTmyA5OkA0NIfYs2I" />
       <body>
         <NextIntlClientProvider>
 
