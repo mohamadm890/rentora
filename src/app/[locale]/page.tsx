e7a07cf270b1928feda4a7e240ed4b7478889d66
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2026-06-10 12:59:45 +0100

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,7 +6,44 @@ import { getListings } from "@/services/clients/property.client";
 
 import {normalizeListing} from '../lib/normalizeListing';
 
+import type { Metadata } from "next";
 
+
+
+export async function generateMetadata(): Promise<Metadata> {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL;
+
+  return {
+    title: "Rentora | Location immobilière au Maroc",
+    description:
+      "Trouvez les meilleures annonces de location d’appartements, villas et studios au Maroc.",
+
+    alternates: {
+      canonical: `${baseUrl}/`,
+
+      languages: {
+        "x-default": `${baseUrl}/`,
+        fr: `${baseUrl}/fr`,
+      },
+    },
+
+    openGraph: {
+      title: "Rentora | Location immobilière au Maroc",
+      description:
+        "Trouvez les meilleures annonces de location d’appartements, villas et studios au Maroc.",
+      url: `${baseUrl}/`,
+      type: "website",
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: "Rentora | Location immobilière au Maroc",
+      description:
+        "Trouvez les meilleures annonces de location d’appartements, villas et studios au Maroc.",
+    },
+  };
+}
 
 export default async function Page() {
   
